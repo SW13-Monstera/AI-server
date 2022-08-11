626a7dc3a9183b3f98bdb028b3223125850120f6
ekzm8523 <mjw8523@gmail.com>
2022-08-11 23:47:32 +0900

    ✅ pre-commit test

--- service.py
+++ service.py
@@ -4,13 +4,7 @@ import bentoml
 from bentoml.io import JSON
 from sklearn.metrics.pairwise import cosine_similarity
 
-from schemas import (
-    Keyword,
-    KeywordInferenceData,
-    KeywordInferenceResponse,
-    KeywordResponse,
-    Problem,
-)
+from schemas import Keyword, KeywordInferenceData, KeywordInferenceResponse, KeywordResponse, Problem
 
 keyword_model = bentoml.pytorch.get("sentence-ko-roberta")
 
@@ -29,10 +23,7 @@ class KeywordPredictRunnable(bentoml.Runnable):
         problem_id = input_data.problem_id
         exist_keywords = self.problem_dict[problem_id].keywords
         remain_keywords = []
-        input_keyword_dict = {
-            input_keyword.id: input_keyword.content
-            for input_keyword in input_data.keywords
-        }
+        input_keyword_dict = {input_keyword.id: input_keyword.content for input_keyword in input_data.keywords}
         for exist_keyword in exist_keywords:
             if exist_keyword.id in input_keyword_dict:
                 input_keyword_dict.pop(exist_keyword.id)
@@ -40,43 +31,31 @@ class KeywordPredictRunnable(bentoml.Runnable):
         is_keyword_changed = len(input_keyword_dict) > 0
         if is_keyword_changed:
             for new_keyword_id, new_keyword_content in input_keyword_dict.items():
-                remain_keywords.append(
-                    Keyword(id=new_keyword_id, content=new_keyword_content)
-                )
+                remain_keywords.append(Keyword(id=new_keyword_id, content=new_keyword_content))
             self.problem_dict[problem_id].keywords = remain_keywords
-            new_embedded_keywords = self.model.encode(
-                [keyword.content for keyword in remain_keywords]
-            )
+            new_embedded_keywords = self.model.encode([keyword.content for keyword in remain_keywords])
             self.problem_dict[problem_id].embedded_keywords = new_embedded_keywords
 
     @bentoml.Runnable.method(batchable=False)
-    def is_correct_keyword(
-        self, input_data: KeywordInferenceData
-    ) -> KeywordInferenceResponse:
+    def is_correct_keyword(self, input_data: KeywordInferenceData) -> KeywordInferenceResponse:
         # 키워드 변경 감지도 추가 해야함
         self.synchronize_keywords(input_data)
         if input_data.problem_id not in self.problem_dict:
             self.problem_dict[input_data.problem_id] = Problem(
                 subject=None,
                 keywords=input_data.keywords,
-                embedded_keywords=self.model.encode(
-                    [keyword.content for keyword in input_data.keywords]
-                ),
+                embedded_keywords=self.model.encode([keyword.content for keyword in input_data.keywords]),
             )
 
         problem = self.problem_dict[input_data.problem_id]
         split_answer = input_data.user_answer.strip().split()
         tokenized_answer = []
         for k in range(len(split_answer) - self.word_concat_size + 1):
-            tokenized_answer.append(
-                " ".join(split_answer[k : k + self.word_concat_size])
-            )
+            tokenized_answer.append(" ".join(split_answer[k : k + self.word_concat_size]))
         if len(split_answer) < self.word_concat_size:
             tokenized_answer.append(" ".join(split_answer))
         tokenized_answer_embedding = self.model.encode(tokenized_answer)
-        similarity_scores = cosine_similarity(
-            problem.embedded_keywords, tokenized_answer_embedding
-        )
+        similarity_scores = cosine_similarity(problem.embedded_keywords, tokenized_answer_embedding)
         predicts = []
         for z, idx in enumerate(similarity_scores.argmax(axis=1)):
             if self.threshold < similarity_scores[z][idx]:
@@ -91,9 +70,7 @@ class KeywordPredictRunnable(bentoml.Runnable):
                     )
                 )
 
-        return KeywordInferenceResponse(
-            problem_id=input_data.problem_id, correct_keywords=predicts
-        )
+        return KeywordInferenceResponse(problem_id=input_data.problem_id, correct_keywords=predicts)
 
 
 keyword_runner = bentoml.Runner(KeywordPredictRunnable, models=[keyword_model])
--- tests/test_keyword.py
+++ tests/test_keyword.py
@@ -6,11 +6,7 @@ from schemas import Keyword, KeywordInferenceData
 from service import KeywordPredictRunnable
 
 
-def test_debug(
-    keyword_runner: Runner,
-    problem_dict: dict,
-    random_keyword_data: KeywordInferenceData,
-):
+def test_debug(keyword_runner: Runner, problem_dict: dict, random_keyword_data: KeywordInferenceData):
 
     keyword_runner.init_local()
 
@@ -19,9 +15,7 @@ def test_debug(
 
 
 @pytest.mark.asyncio
-async def test_keyword_inference(
-    host: str, random_keyword_data: KeywordInferenceData
-) -> None:
+async def test_keyword_inference(host: str, random_keyword_data: KeywordInferenceData) -> None:
     """
     api response 200 test
     """
@@ -34,9 +28,7 @@ async def test_keyword_inference(
     )
 
 
-def test_keyword_predict_runnable(
-    problem_dict: dict, random_keyword_data: KeywordInferenceData
-) -> None:
+def test_keyword_predict_runnable(problem_dict: dict, random_keyword_data: KeywordInferenceData) -> None:
     """
     로컬 메모리에 problem 정보가 없을 때 새롭게 problem에 대한 키워드 정보를 생성하고 예측한다.
     """
@@ -48,21 +40,15 @@ def test_keyword_predict_runnable(
     assert test_problem_id not in runnable.problem_dict
     result = runnable.is_correct_keyword(random_keyword_data)
     assert test_problem_id in runnable.problem_dict
-    assert (
-        random_keyword_data.keywords == runnable.problem_dict[test_problem_id].keywords
-    )
+    assert random_keyword_data.keywords == runnable.problem_dict[test_problem_id].keywords
     assert test_problem_id == result.problem_id
 
-    problem_keyword_set = set(
-        keyword.id for keyword in problem_dict[random_keyword_data.problem_id].keywords
-    )
+    problem_keyword_set = set(keyword.id for keyword in problem_dict[random_keyword_data.problem_id].keywords)
     for correct_keyword in result.correct_keywords:
         assert correct_keyword.id in problem_keyword_set
 
 
-def test_keyword_predict_runnable_2(
-    problem_dict: dict, random_keyword_data: KeywordInferenceData
-) -> None:
+def test_keyword_predict_runnable_2(problem_dict: dict, random_keyword_data: KeywordInferenceData) -> None:
     """
     기존에 있던 키워드가 하나 지워지고 새로 생겼을 때 메모리에서도 지우고 새로 생긴 키워드를 임베딩해서 저장한다.
     """
@@ -77,21 +63,13 @@ def test_keyword_predict_runnable_2(
     random_keyword_data.keywords.append(new_keyword)  # 키워드 하나 추가
     result = runnable.is_correct_keyword(random_keyword_data)
 
-    problem_keyword_id_set = set(
-        keyword.id for keyword in runnable.problem_dict[test_problem_id].keywords
-    )
-    assert (
-        delete_keyword.id not in problem_keyword_id_set
-    ), "삭제되어야 할 키워드가 메모리에서 지워지지 않았습니다."
+    problem_keyword_id_set = set(keyword.id for keyword in runnable.problem_dict[test_problem_id].keywords)
+    assert delete_keyword.id not in problem_keyword_id_set, "삭제되어야 할 키워드가 메모리에서 지워지지 않았습니다."
     assert new_keyword.id in problem_keyword_id_set, "새로 생긴 키워드가 메모리에 저장되지 않았습니다."
 
-    assert (
-        random_keyword_data.keywords == runnable.problem_dict[test_problem_id].keywords
-    ), "키워드 동기화가 되지 않았습니다."
+    assert random_keyword_data.keywords == runnable.problem_dict[test_problem_id].keywords, "키워드 동기화가 되지 않았습니다."
     assert test_problem_id == result.problem_id, "request와 response의 문제 ID가 같지 않습니다."
 
-    problem_keyword_set = set(
-        keyword.id for keyword in runnable.problem_dict[test_problem_id].keywords
-    )
+    problem_keyword_set = set(keyword.id for keyword in runnable.problem_dict[test_problem_id].keywords)
     for correct_keyword in result.correct_keywords:
         assert correct_keyword.id in problem_keyword_set
