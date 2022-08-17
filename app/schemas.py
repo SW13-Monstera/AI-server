84735bdae767720d53a28a9f06cc0a0528feb07b
ekzm8523 <mjw8523@gmail.com>
2022-08-17 20:17:12 +0900

    :construction_worker: Add exception and pytest config file
    add exception and pytest config file

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,20 @@
-from typing import List, Optional
+from typing import List
 
+from bentoml.exceptions import BadInput
 from numpy.typing import NDArray
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Keyword(BaseModel):
     id: int
     content: str
+
+    @validator("content")
+    def validate_content(cls, value: str) -> str:
+        value = value.strip()
+        if value == "":
+            raise BadInput("Keyword content cannot be empty")
+        return value
 
 
 class KeywordInferenceRequest(BaseModel):
@@ -14,11 +22,35 @@ class KeywordInferenceRequest(BaseModel):
     user_answer: str
     keywords: List[Keyword]
 
+    @validator("user_answer")
+    def validate_user_answer(cls, value: str) -> str:
+        value = value.strip()
+        if value == "":
+            raise BadInput("User answer cannot be empty")
+        return value
+
+    @validator("keywords")
+    def validate_keywords(cls, value: List[Keyword]) -> List[Keyword]:
+        if not value:
+            raise BadInput("Keywords cannot be empty")
+        return value
+
 
 class Problem(BaseModel):
-    subject: Optional[str]
     keywords: List[Keyword]
     embedded_keywords: NDArray
+
+    @validator("keywords")
+    def validate_keywords(cls, value: List[Keyword]) -> List[Keyword]:
+        if not value:
+            raise BadInput("Keywords cannot be empty")
+        return value
+
+    @validator("embedded_keywords")
+    def validate_embedded_keywords(cls, value: NDArray, values: dict) -> NDArray:
+        if len(values["keywords"]) != value.shape[0]:
+            raise BadInput("Keywords and embedded_keywords must have the same length")
+        return value
 
     class Config:
         arbitrary_types_allowed = True
