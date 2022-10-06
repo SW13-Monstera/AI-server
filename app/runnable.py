aadac8e0432a21e3acc38cbaa4c4f1a2de7cdd96
ekzm8523 <mjw8523@gmail.com>
2022-10-06 16:15:19 +0900

    :truck: reflect kshired code review
    reflect kshired code review

--- app/runnable.py
+++ app/runnable.py
@@ -64,8 +64,8 @@ class KeywordPredictRunnable(bentoml.Runnable):
                 self.create_problem(input_data)
 
     def get_tokenized_answer(self, user_answer: str) -> List[str]:
-        special_char_pattern = r"[^\uAC00-\uD7A3a-zA-Z\s]"
-        user_answer = re.sub(special_char_pattern, "", user_answer)
+        regex_filter = r"[^\uAC00-\uD7A3a-zA-Z\s]"
+        user_answer = re.sub(regex_filter, "", user_answer)
         tokenized_answers = tuple(
             word for word, _ in self.tokenizer.pos(user_answer) if word not in self.stopwords and len(word) > 1
         )
--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,14 +1,8 @@
+from pathlib import Path
 from typing import Set
 
 from app.config import STOPWORD_FILE_PATH
 
 
 def get_stopwords() -> Set:
-    stop_word = set()
-    file = open(STOPWORD_FILE_PATH, "r")
-    while True:
-        line = file.readline()
-        if not line:
-            break
-        stop_word.add(line.strip())
-    return stop_word
+    return set(Path(STOPWORD_FILE_PATH).read_text().split("\n"))
