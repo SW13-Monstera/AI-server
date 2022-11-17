9597a79367975e0b7b0cb8345d7a32df8761a028
ekzm8523 <mjw8523@gmail.com>
2022-11-17 22:41:17 +0900

    :zap: add model cache
    add model cache

--- app/api/dependency.py
+++ app/api/dependency.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import Optional
 
 import torch
@@ -8,10 +9,17 @@ from openprompt.plms import get_model_class
 from openprompt.prompts import ManualTemplate, ManualVerbalizer
 from sentence_transformers import SentenceTransformer
 
-from app.config import CONTENT_MODEL_NAME, CONTENT_MODEL_PATH, CONTENT_MODEL_S3_PATH, KEYWORD_MODEL_PATH
+from app.config import (
+    CONTENT_MODEL_NAME,
+    CONTENT_MODEL_PATH,
+    CONTENT_MODEL_S3_PATH,
+    KEYWORD_LOCAL_MODEL_PATH,
+    KEYWORD_MODEL_PATH,
+)
 from app.controller.content import ContentController
 from app.controller.keyeword import KeywordController
 from app.utils.aws_s3 import AwsS3Downloader
+from app.utils.utils import get_template_text
 
 log = logging.getLogger("__main__")
 s3 = AwsS3Downloader()
@@ -23,17 +31,22 @@ content_model: Optional[PromptForClassification] = None
 def init_model() -> None:
     global keyword_model, content_model
 
-    keyword_model = SentenceTransformer(KEYWORD_MODEL_PATH)
+    if os.path.isfile(KEYWORD_LOCAL_MODEL_PATH):
+        keyword_model = SentenceTransformer()
+        keyword_model.load_state_dict(torch.load(KEYWORD_LOCAL_MODEL_PATH))
+    else:
+        keyword_model = SentenceTransformer(KEYWORD_MODEL_PATH)
+        torch.save(keyword_model.state_dict(), KEYWORD_LOCAL_MODEL_PATH)
 
     model_class = get_model_class(plm_type=CONTENT_MODEL_NAME)
     plm = model_class.model.from_pretrained(CONTENT_MODEL_PATH)
     tokenizer = model_class.tokenizer.from_pretrained(CONTENT_MODEL_PATH)
-
-    template_text = '{"placeholder":"text_a"} Question: {"placeholder":"text_b"}? Is it correct? {"mask"}.'
+    template_text = get_template_text()
     template = ManualTemplate(tokenizer=tokenizer, text=template_text)
     verbalizer = ManualVerbalizer(tokenizer=tokenizer, num_classes=2, label_words=[["yes"], ["no"]])
 
     content_model = PromptForClassification(plm=plm, template=template, verbalizer=verbalizer)
+
     model_path = s3.download(url=CONTENT_MODEL_S3_PATH, local_dir=".cache")
     content_model.load_state_dict(torch.load(model_path, map_location=device))
 
--- app/utils/utils.py
+++ app/utils/utils.py
@@ -6,3 +6,7 @@ from app.config import STOPWORD_FILE_PATH
 
 def get_stopwords() -> Set:
     return set(Path(STOPWORD_FILE_PATH).read_text(encoding="UTF-8").split("\n"))
+
+
+def get_template_text() -> str:
+    return '{"placeholder":"text_a"} Question: {"placeholder":"text_b"}? Is it correct? {"mask"}.'
